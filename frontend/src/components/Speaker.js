885091aacfcfa57f249c3ba977e9b350383ea1ca
ThanaphonKeawjam <tongasura001@gmail.com>
2018-12-05 12:15:07 +0700

    fixed selenium error

--- frontend/src/components/Speaker.js
+++ frontend/src/components/Speaker.js
@@ -194,8 +194,8 @@ class Speaker extends Component {
                        'end_time':etime, 'speaker':speak, 'room':r, 'vote':v }
     $.ajax({
       dataType: 'json',
-      url: 'https://barcamp-management.herokuapp.com/api/topic/',
-      // url: 'http://localhost:3000/api/topic/',
+      // url: 'https://barcamp-management.herokuapp.com/api/topic/',
+      url: 'http://localhost:3000/api/topic/',
       type: 'POST',
       data: JSON.stringify(sendData),
       contentType:'application/json',
@@ -218,8 +218,8 @@ class Speaker extends Component {
   }
 
   componentDidMount() {
-    fetch("https://barcamp-management.herokuapp.com/api/topic/")
-    // fetch("http://localhost:3000/api/topic/")
+    // fetch("https://barcamp-management.herokuapp.com/api/topic/")
+    fetch("http://localhost:3000/api/topic/")
       .then(response => {
         if (response.status !== 200) {
           return console.log('error')
