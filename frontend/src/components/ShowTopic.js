76bf0bd2a55e8af8bf9274cd0c95b2daf7e4682e
ThanaphonKeawjam <tongasura001@gmail.com>
2018-12-05 12:00:39 +0700

    update

--- frontend/src/components/ShowTopic.js
+++ frontend/src/components/ShowTopic.js
@@ -22,8 +22,8 @@ class ShowTopic extends Component{
 
 
     componentDidMount(){
-        // fetch("https://barcamp-management.herokuapp.com/api/topic/")
-        fetch("http://localhost:3000/api/topic/")
+        fetch("https://barcamp-management.herokuapp.com/api/topic/")
+        // fetch("http://localhost:3000/api/topic/")
             .then(response => {
                 if (response.status !== 200) {
                     return console.log('error')
